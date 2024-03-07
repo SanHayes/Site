838e82b4215d121a128b9977d88d6b9c91242b00
SanHayes <126791399+SanHayes@users.noreply.github.com>
2024-03-07 12:03:57 +0800

    Update functions.php

--- common/functions.php
+++ common/functions.php
@@ -44,6 +44,41 @@ function GoogleVerify($secret, $code)
     return ['result' => $checkResult];
 }
 
+function curlfun($url, $params = array(), $method = 'GET'){
+    $header = array();
+    $opts = array(CURLOPT_TIMEOUT => 10, CURLOPT_RETURNTRANSFER => 1, CURLOPT_SSL_VERIFYPEER => false, CURLOPT_SSL_VERIFYHOST => false, CURLOPT_HTTPHEADER => $header);
+    
+    /* 根据请求类型设置特定参数 */
+    switch (strtoupper($method)) {
+            case 'GET' :
+                    $opts[CURLOPT_URL] = $url . '?' . http_build_query($params);
+                    $opts[CURLOPT_URL] = substr($opts[CURLOPT_URL],0,-1);
+                    
+                    break;
+            case 'POST' :
+                    //判断是否传输文件
+                    $params = http_build_query($params);
+                    $opts[CURLOPT_URL] = $url;
+                    $opts[CURLOPT_POST] = 1;
+                    $opts[CURLOPT_POSTFIELDS] = $params;
+                    break;
+            default :
+                    break;
+    }
+    
+    /* 初始化并执行curl请求 */
+    $ch = curl_init();
+    curl_setopt_array($ch, $opts);
+    $data = curl_exec($ch);
+    $error = curl_error($ch);
+    curl_close($ch);
+    
+    if($error){
+            $data = null;
+    }		
+    return $data;
+}
+
 function mtranslate($val, $to, $from = 'zh')
 {
     require_once dirname(__DIR__).'/public/translate.php';
